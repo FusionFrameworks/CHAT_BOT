70a808014c8abc61f367c95740a1c00c1bfd8aee
PradyLinux <pradyumna.p.6969@gmail.com>
2024-12-10 22:50:13 +0530

    fix: Added roomId attribute to the paymentSchema

--- ChatBot_Backend/models/paymentModel.js
+++ ChatBot_Backend/models/paymentModel.js
@@ -1,16 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Payment Schema
 const paymentSchema = new mongoose.Schema({
-    paymentId: { type: String, unique: true },
-    patientId: { type: String, required: false },
-    name: String,
-    amount: Number,
-    status: { type: String, enum: ['pending', 'completed', 'failed'], default: 'pending' },
-    createdAt: { type: Date, default: Date.now }
+  paymentId: { type: String, unique: true },
+  patientId: { type: String, required: false },
+  name: String,
+  amount: Number,
+  roomId: { type: Number, required: false },
+  status: {
+    type: String,
+    enum: ["pending", "completed", "failed"],
+    default: "pending",
+  },
+  createdAt: { type: Date, default: Date.now },
 });
 
 // Export the Payment model
-const Payment = mongoose.model('Payment', paymentSchema);
+const Payment = mongoose.model("Payment", paymentSchema);
 
 module.exports = Payment;
